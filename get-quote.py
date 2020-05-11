fa52b743b162cb09f242f41fcb3ed1887e0ee8bc
clyhess <65195737+clyhess@users.noreply.github.com>
2020-05-11 18:13:25 -0400

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,12 @@
+Import Random
 def main(Wordly):
 	print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-
+ last = 13
+  rnd = random.randint(0, last)
   print(quotes[13])
 
 if __name__== "__main__":
